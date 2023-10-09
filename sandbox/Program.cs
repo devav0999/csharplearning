e3c4934806bce9832137217e94962bf8789a54ef
devav0999 <devav0999@dev.com>
2023-10-09 21:50:54 +0200

    adding two numbers

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Title = "adding two numbers";
+
+string first_number;
+string second_number;
+
+
+Console.WriteLine("write first number");
+first_number = Console.ReadLine();
+
+Console.WriteLine("write second number");
+second_number = Console.ReadLine();
+
+
+try
+{
+    Console.WriteLine("sum of numbers");
+    Console.WriteLine(Double.Parse(first_number) + Double.Parse(second_number));
+    
+}
+catch (FormatException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+Console.WriteLine("Press enter to close...");
+Console.ReadLine();
